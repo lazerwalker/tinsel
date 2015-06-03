fe7959a0c4c2a3486ec70cf7288d54d93c6f1f5e
Mike Walker <michael@lazerwalker.com>
2015-06-03 14:05:20 -0700

    Connect URLs to nodes

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
+var _ = require('lodash')
+
 var fs = require('fs')
 var data = JSON.parse(fs.readFileSync('example.json', 'utf8'))
-
-var app = require('express').express();
+var app = require('express')();
 var server = app.listen(3000)
 
 app.get('/:slug', function (req, res) {
-  res.send(req.params.slug);
+  var node = _(data.story).findWhere({'name': req.params.slug})
+  res.send(renderNode(node, res));
 });
 
+app.get('/', function(req, res) {
+  var node = _(data.story).findWhere({'name': data.start})
+  res.send(renderNode(node, res));
+});
+
+function renderNode(node) {
+  return JSON.stringify(node);
+}
