5215bcdd52aaa1bbc3698bc948d10d18aada08f5
Mike Walker <michael@lazerwalker.com>
2015-06-03 13:58:03 -0700

    Basic express setup

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-fs = require('fs')
-fs.readFile('example.json', 'utf8', function (err,data) {
-  console.log(JSON.parse(data));
+var fs = require('fs')
+var data = JSON.parse(fs.readFileSync('example.json', 'utf8'))
+
+var app = require('express').express();
+var server = app.listen(3000)
+
+app.get('/:slug', function (req, res) {
+  res.send(req.params.slug);
 });
+
