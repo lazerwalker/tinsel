87b7f08f0f82f1f610f50be03eec80b8c70492c1
Mike Walker <michael@lazerwalker.com>
2015-06-03 14:09:22 -0700

    Add semicolons

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-var _ = require('lodash')
+var _ = require('lodash');
 
 var fs = require('fs')
-var data = JSON.parse(fs.readFileSync('example.json', 'utf8'))
+var data = JSON.parse(fs.readFileSync('example.json', 'utf8'));
 var app = require('express')();
-var server = app.listen(3000)
+var server = app.listen(3000);
 
 app.get('/:slug', function (req, res) {
-  var node = _(data.story).findWhere({'name': req.params.slug})
+  var node = _(data.story).findWhere({'name': req.params.slug});
   res.send(renderNode(node, res));
 });
 
 app.get('/', function(req, res) {
-  var node = _(data.story).findWhere({'name': data.start})
+  var node = _(data.story).findWhere({'name': data.start});
   res.send(renderNode(node, res));
 });
 
