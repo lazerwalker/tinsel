28c164997d75ecb6f42176b9310bcc9e2731d819
Mike Walker <michael@lazerwalker.com>
2015-06-03 14:13:42 -0700

    Generate (very basic, incomplete) TwiML

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var _ = require('lodash');
+var Twilio = require('twilio');
 
 var fs = require('fs')
 var data = JSON.parse(fs.readFileSync('example.json', 'utf8'));
@@ -7,14 +8,20 @@ var server = app.listen(3000);
 
 app.get('/:slug', function (req, res) {
   var node = _(data.story).findWhere({'name': req.params.slug});
-  res.send(renderNode(node, res));
+
+  res.set('Content-Type', 'text/html');
+  res.send(renderNode(node));
 });
 
 app.get('/', function(req, res) {
   var node = _(data.story).findWhere({'name': data.start});
-  res.send(renderNode(node, res));
+
+  res.set('Content-Type', 'text/html');
+  res.send(renderNode(node));
 });
 
 function renderNode(node) {
-  return JSON.stringify(node);
+  var resp = new Twilio.TwimlResponse();
+  resp.say(node.content);
+  return resp.toString();
 }
