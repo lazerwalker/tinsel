b9b556ae1927b5804ccab9cb4bcb857de32273a3
Mike Walker <michael@lazerwalker.com>
2015-06-03 19:54:24 -0700

    ES6-ify all the things

--- main.js
+++ main.js
@@ -1,78 +1,76 @@
-var _ = require('lodash');
-var Twilio = require('twilio');
-var Sandbox = require('sandbox');
-var Q = require('q');
-var fs = require('fs')
+const _ = require('lodash');
+const Twilio = require('twilio');
+const Sandbox = require('sandbox');
+const Q = require('q');
+const fs = require('fs')
 
 var data;
-var script = fs.readFileSync('example.json', 'utf8');
-var sandbox = fs.readFileSync('sandbox.js', 'utf8')
+const script = fs.readFileSync('example.json', 'utf8');
+const sandbox = fs.readFileSync('sandbox.js', 'utf8')
     .replace("{{SCRIPT}}", script);
 
 function runSandbox(message, callback) {
-    console.log("Running sandbox with " + message)
-    var s = new Sandbox();
+    const s = new Sandbox();
     s.run(sandbox);
     s.on('message', callback);
     s.postMessage(message);
 }
 
-runSandbox("tree", function(message) {
+runSandbox("tree", (message) => {
     data = message;
 });
 
 
+const app = require('express')();
+const server = app.listen(3000);
 
-var app = require('express')();
-var server = app.listen(3000);
-
-app.get('/:slug', function (req, res) {
+app.get('/:slug', (req, res) => {
     var node = _(data.story).findWhere({'name': req.params.slug});
 
     if (req.query["Digits"]) {
-        var newNodeName = node.routes[req.query["Digits"]];
-        var newNode = _(data.story).findWhere({'name': newNodeName});
+        const newNodeName = node.routes[req.query["Digits"]];
+        const newNode = _(data.story).findWhere({'name': newNodeName});
         if (newNode) { node = newNode; }
     }
 
-    renderNode(node, req.query).then(function(xml) {
+    renderNode(node, req.query).then( (xml) => {
         sendResponse(xml, res);
     });
 });
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
     res.redirect("/" + data.start);
 });
 
 function renderNode(node, data) {
-    var response = new Twilio.TwimlResponse();
+    const response = new Twilio.TwimlResponse();
 
     // returns a promise containing a new node.content
     function unwrapFunctions() {
         function unwrapFunction(content) {
             if (content.type !== "function") return Q(content);
 
-            var defer = Q.defer();
+            const defer = Q.defer();
 
-            var message = JSON.stringify({
+            const message = JSON.stringify({
                 functionCount: content.functionCount,
                 opts: data
             });
-            runSandbox(message, function(newContent) {
+            runSandbox(message, (newContent) => {
                 defer.resolve(newContent);            
             });
             return defer.promise;
         }
 
         if (_(node.content).isArray()) {
-            var promises = node.content.map(unwrapFunction);
+            const promises = node.content.map(unwrapFunction);
             return Q.all(promises);
         } else {
             return unwrapFunction(node.content);
         }
     }
 
-    return unwrapFunctions().then(function (content) {
+    return unwrapFunctions().then( (content) => {
         function sayText(n) {
             function handleObj(obj) {
                 if (_.isArray(obj)) {
@@ -86,7 +84,7 @@ function renderNode(node, data) {
                     if (obj.type == "pause") {
                         n.pause(opts);
                     } else if (obj.type == "function") {
-                        runSandbox(obj.functionCount, function(m) {
+                        runSandbox(obj.functionCount, (m) => {
                             handleObj(m);            
                         });
                     } else {
@@ -99,12 +97,12 @@ function renderNode(node, data) {
         }
 
         if (node.routes) {
-            var defaultGatherOpts = {
+            const defaultGatherOpts = {
                 method: "GET",
                 numDigits: 1
             };
 
-            var gatherOptions = _.assign(defaultGatherOpts, node.gatherOptions);
+            const gatherOptions = _.assign(defaultGatherOpts, node.gatherOptions);
             response.gather(gatherOptions, sayText);
 
         } else {
