234357c8b3530fe66c687a7c28ecb3f886d58ac8
Mike Walker <michael@lazerwalker.com>
2015-06-03 19:19:43 -0700

    Switch to 4 spaces

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ var fs = require('fs')
 var data;
 var script = fs.readFileSync('example.json', 'utf8');
 var sandbox = fs.readFileSync('sandbox.js', 'utf8')
-  .replace("{{SCRIPT}}", script);
+    .replace("{{SCRIPT}}", script);
 
 function runSandbox(message, callback) {
-  console.log("Running sandbox with " + message)
-  var s = new Sandbox();
-  s.run(sandbox);
-  s.on('message', callback);
-  s.postMessage(message);
+    console.log("Running sandbox with " + message)
+    var s = new Sandbox();
+    s.run(sandbox);
+    s.on('message', callback);
+    s.postMessage(message);
 }
 
 runSandbox("tree", function(message) {
-  data = message;
+    data = message;
 });
 
 
@@ -27,96 +27,96 @@ var app = require('express')();
 var server = app.listen(3000);
 
 app.get('/:slug', function (req, res) {
-  var node = _(data.story).findWhere({'name': req.params.slug});
+    var node = _(data.story).findWhere({'name': req.params.slug});
 
-  if (req.query["Digits"]) {
-    var newNodeName = node.routes[req.query["Digits"]];
-    var newNode = _(data.story).findWhere({'name': newNodeName});
-    if (newNode) { node = newNode; }
-  }
+    if (req.query["Digits"]) {
+        var newNodeName = node.routes[req.query["Digits"]];
+        var newNode = _(data.story).findWhere({'name': newNodeName});
+        if (newNode) { node = newNode; }
+    }
 
-  renderNode(node, req.query).then(function(xml) {
-    sendResponse(xml, res);
-  });
+    renderNode(node, req.query).then(function(xml) {
+        sendResponse(xml, res);
+    });
 });
 
 app.get('/', function(req, res) {
-  res.redirect("/" + data.start);
+    res.redirect("/" + data.start);
 });
 
 function renderNode(node, data) {
-  var response = new Twilio.TwimlResponse();
+    var response = new Twilio.TwimlResponse();
 
-  // returns a promise containing a new node.content
-  function unwrapFunctions() {
-    function unwrapFunction(content) {
-      if (content.type !== "function") return Q(content);
+    // returns a promise containing a new node.content
+    function unwrapFunctions() {
+        function unwrapFunction(content) {
+            if (content.type !== "function") return Q(content);
 
-      var defer = Q.defer();
+            var defer = Q.defer();
 
-      var message = JSON.stringify({
-        functionCount: content.functionCount,
-        opts: data
-      });
-      runSandbox(message, function(newContent) {
-        defer.resolve(newContent);            
-      });
-      return defer.promise;
-    }
-
-    if (_(node.content).isArray()) {
-      var promises = node.content.map(unwrapFunction);
-      return Q.all(promises);
-    } else {
-      return unwrapFunction(node.content);
-    }
-  }
-
-  return unwrapFunctions().then(function (content) {
-    function sayText(n) {
-      function handleObj(obj) {
-        if (_.isArray(obj)) {
-          _.each(obj, function(o) { handleObj(o); });
-        } else if (_.isString(obj)) {
-          n.say(obj);
-        } else {
-          var opts = _.clone(obj);
-          delete opts.type
-
-          if (obj.type == "pause") {
-            n.pause(opts);
-          } else if (obj.type == "function") {
-            runSandbox(obj.functionCount, function(m) {
-              handleObj(m);            
+            var message = JSON.stringify({
+                functionCount: content.functionCount,
+                opts: data
             });
-          } else {
-            delete opts.text
-            n.say(obj.text, opts)
-          }
+            runSandbox(message, function(newContent) {
+                defer.resolve(newContent);            
+            });
+            return defer.promise;
         }
-      }
-      handleObj(content);
+
+        if (_(node.content).isArray()) {
+            var promises = node.content.map(unwrapFunction);
+            return Q.all(promises);
+        } else {
+            return unwrapFunction(node.content);
+        }
     }
 
-    if (node.routes) {
-      var defaultGatherOpts = {
-        method: "GET",
-        numDigits: 1
-      };
+    return unwrapFunctions().then(function (content) {
+        function sayText(n) {
+            function handleObj(obj) {
+                if (_.isArray(obj)) {
+                    _.each(obj, function(o) { handleObj(o); });
+                } else if (_.isString(obj)) {
+                    n.say(obj);
+                } else {
+                    var opts = _.clone(obj);
+                    delete opts.type
 
-      var gatherOptions = _.assign(defaultGatherOpts, node.gatherOptions);
-      response.gather(gatherOptions, sayText);
+                    if (obj.type == "pause") {
+                        n.pause(opts);
+                    } else if (obj.type == "function") {
+                        runSandbox(obj.functionCount, function(m) {
+                            handleObj(m);            
+                        });
+                    } else {
+                        delete opts.text
+                        n.say(obj.text, opts)
+                    }
+                }
+            }
+            handleObj(content);
+        }
 
-    } else {
-      sayText(response);
-    }
+        if (node.routes) {
+            var defaultGatherOpts = {
+                method: "GET",
+                numDigits: 1
+            };
 
-    return Q(response.toString());
-  });
+            var gatherOptions = _.assign(defaultGatherOpts, node.gatherOptions);
+            response.gather(gatherOptions, sayText);
+
+        } else {
+            sayText(response);
+        }
+
+        return Q(response.toString());
+    });
 }
 
 function sendResponse(text, res) {
-  res.set('Content-Type', 'text/xml');
-  // console.log(text);
-  res.send(text);
+    res.set('Content-Type', 'text/xml');
+    // console.log(text);
+    res.send(text);
 }
