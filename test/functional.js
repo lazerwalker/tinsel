7ec2a361437c40211d0e6acbe46f745e1d4b2fb5
Mike Walker <michael@lazerwalker.com>
2015-06-03 20:29:59 -0700

    Fix wonky tests

--- test/functional.js
+++ test/functional.js
@@ -35,15 +35,14 @@ describe("when using the example JSON file", function() {
 
     describe("when passing parameters", function(done) {
         it("should persist through a Digit redirect", function(done) {
-            request("http://localhost:3000/first?foo=Applesauce", function(error, response, body) {
-                expect(body).to.equal('<?xml version="1.0" encoding="UTF-8"?><Response><Gather method="GET" numDigits="3" finishOnKey="*"><Say voice="alice" language="en">This is the first node</Say><Pause length="2"></Pause><Say>The value of query param &apos;foo&apos; is Applesauce</Say><Say voice="man">Please make a selection!</Say></Gather></Response>');
+            request("http://localhost:3000/first?Digits=1&foo=Applesauce", function(error, response, body) {
+                expect(body).to.equal('<?xml version="1.0" encoding="UTF-8"?><Response><Say>This is the second node. Foo is Applesauce</Say></Response>');
                 done();
             });        
         });
 
         it("should persist through a root redirect", function(done) {
             request("http://localhost:3000/?foo=Applesauce", function(error, response, body) {
-                expect(body).to.exist;
                 expect(body).to.equal('<?xml version="1.0" encoding="UTF-8"?><Response><Gather method="GET" numDigits="3" finishOnKey="*"><Say voice="alice" language="en">This is the first node</Say><Pause length="2"></Pause><Say>The value of query param &apos;foo&apos; is Applesauce</Say><Say voice="man">Please make a selection!</Say></Gather></Response>');
                 done();
             });        
